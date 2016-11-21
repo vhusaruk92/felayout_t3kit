9c74e3dd93a1f673dca13447c5bc6d80a9161235
Viktoria Husaruk <vhusaruk92@gmail.com>
2016-11-21 12:00:49 +0200

    [FEATURE]add slider container

--- dev/js/main.js
+++ dev/js/main.js
@@ -6,7 +6,7 @@
 @import 'main/contentElements/slider.js';
 @import 'main/contentElements/carousel.js';
 @import 'main/contentElements/parallax.js';
-@import 'main/contentElements/swiperContainer.js';
+@import 'main/contentElements/sliderContainer.js';
 
 // plugins
 @import 'main/plugins/news/newsCarousel.js';
